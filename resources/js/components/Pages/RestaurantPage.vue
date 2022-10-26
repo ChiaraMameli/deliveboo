f6783fef6694c0268010ddd334288e9f843a4ee3
FRANCO1357 <fmelani13@gmail.com>
2022-10-26 18:33:48 +0200

    setted localStorage

--- resources/js/components/Pages/RestaurantPage.vue
+++ resources/js/components/Pages/RestaurantPage.vue
@@ -1,7 +1,13 @@
 <template>
   <main>
     <div class="container">
-      <h2>Restaurant Page</h2>
+      <h2>{{restaurant.name}}</h2>
+      <ul>
+        <li @click="addToCart(dish)" v-for="dish in restaurant.dishes" :key="dish.id">{{dish.name}}</li>
+      </ul>
+    </div>
+    <div>
+      <p v-for="item in cart">{{item}}</p>
     </div>
   </main>
 </template>
@@ -9,7 +15,43 @@
 <script>
 export default {
 name:'RestaurantPage',
-
+data(){
+  return{
+    restaurant: null,
+    pippo: '',
+    cart: [],
+  }
+},
+methods: {
+        fetchRestaurant() {
+            axios.get("http://localhost:8000/api/restaurants/" + this.$route.params.id).then((res) => {
+                this.restaurant = res.data.restaurant;
+            }).catch(err => {
+                console.log(err);
+            });
+        },
+        addToCart(dish){
+          console.log(dish);
+          console.log(this.cart);
+          if(this.cart.includes(dish)){
+            this.cart.push({'dish': dish.id, 'quantity': 1});
+          } else{
+            dish['quantity'] = dish['quantity'] + 1;
+          }
+          
+        }
+    },
+    mounted() {
+        this.fetchRestaurant();
+        if(localStorage.cart){
+            this.cart = JSON.parse(localStorage.cart);
+        }
+    },
+    watch:{
+        cart(newCart){
+            localStorage.cart = JSON.stringify(newCart);
+        }
+    }
 }
 </script>
 
