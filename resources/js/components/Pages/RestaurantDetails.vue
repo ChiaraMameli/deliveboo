d91d6cd7d533b916b6b0a20c5895e08fdde23f3f
FRANCO1357 <fmelani13@gmail.com>
2022-10-27 14:18:17 +0200

    restaurant-page card

--- resources/js/components/Pages/RestaurantDetails.vue
+++ resources/js/components/Pages/RestaurantDetails.vue
@@ -1,13 +1,22 @@
 <template>
-  <main>
+  <main id="restaurant-details">
     <div class="container">
-      <h2>{{restaurant.name}}</h2>
-      <ul>
-        <li @click="addToCart(dish)" v-for="dish in restaurant.dishes" :key="dish.id">{{dish.name}}</li>
+      <h2 class="text-white text-center p-5">{{restaurant.name}}</h2>
+      <ul class="d-flex flex-wrap justify-content-center list-unstyled">
+        <li class="p-3" v-for="dish in restaurant.dishes" :key="dish.id">
+          <div class="card" style="width: 18rem;">
+            <img :src="dish.image" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">{{dish.name}}</h5>
+              <p class="card-text">{{dish.description}}</p>
+              <i @click="addToCart(dish)" class="fa-solid fa-plus btn btn-danger"></i>
+            </div>
+          </div>
+        </li>
       </ul>
     </div>
     <div>
-      <p v-for="item in cart" :key="item">{{item}}</p>
+      <p class="text-white" v-for="item in cart" :key="item">{{item}}</p>
     </div>
   </main>
 </template>
@@ -56,6 +65,12 @@ methods: {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  #restaurant-details{
+    background-color: rgb(188, 33, 33);
+    
+    i{
+      cursor: pointer;
+    }
+  }
 </style>
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -12,7 +12,10 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
      <!-- Styles -->
-     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    {{-- font-awesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <script src="{{asset('js/front.js')}}" defer></script>
 </head>
