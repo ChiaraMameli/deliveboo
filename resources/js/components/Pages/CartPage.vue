4d24e5102e8a3a66424ba4e2a15d774676820fd9
FRANCO1357 <fmelani13@gmail.com>
2022-10-28 10:14:58 +0200

    updated cart

--- resources/js/components/Pages/CartPage.vue
+++ resources/js/components/Pages/CartPage.vue
@@ -1,7 +1,58 @@
 <template>
     <main id="cart">
-        <div class="container">
-            
+        <div class="jumbotron">
+            <img src="../../../image/deliveboo-cart.png" alt="">
+        </div>
+        <div class="container pt-3 pb-5">
+            <div class="card cart p-5 mt-5">
+                <table class="table">
+                    <thead class="bg-danger text-white">
+                        <tr>
+                        <th scope="col"></th>
+                        <th scope="col">Piatto</th>
+                        <th scope="col">Prezzo</th>
+                        <th scope="col">Quantità</th>
+                        <th scope="col">Sub-totale</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="dish in cart" :key="dish.id">
+                        <th scope="row"><i @click="removeDish(dish)" class="fa-solid fa-xmark"></i></th>
+                        <td>{{dish.dish}}</td>
+                        <td>{{price}}€</td>
+                        <td><input @change="getCurrentQuantity(dish)" type="number" :value="dish.quantity" id="quantity"></td>
+                        <td>{{getSubTotal(dish)}}€</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div>
+                    <i class="fa-solid fa-rotate-left btn btn-primary updated">Aggiorna carrello</i>
+                </div>
+            </div>
+
+            <div class="card cart p-5 mt-5">
+                <form>
+                    <div class="form-group">
+                        <label for="name">Nome e cognome</label>
+                        <input type="text" class="form-control" id="name">
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="email">Indirizzo email</label>
+                            <input type="email" class="form-control" id="email">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="phone">Numero di telefono</label>
+                            <input type="phone" class="form-control" id="phone">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="address">Indirizzo completo</label>
+                        <input type="text" class="form-control" id="address">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Ordina</button>
+                </form>
+            </div>
         </div>
     </main>
 </template>
@@ -12,6 +63,24 @@ export default{
     data(){
         return{
             cart: [],
+            price: 7,
+        }
+    },
+    methods:{
+        removeDish(dish){
+            console.log(dish);
+            this.cart = this.cart.filter(item => item !== dish);
+            localStorage.cart = JSON.stringify(this.cart);
+        },
+        getSubTotal(dish){
+            let subTotal = 0;
+            subTotal = this.price * dish.quantity;
+            return subTotal;
+        },
+        getCurrentQuantity(dish){
+            const inputValue = document.getElementById('quantity');
+            console.log(inputValue.value);
+            dish.quantity = inputValue.value;
         }
     },
     mounted(){
@@ -25,6 +94,28 @@ export default{
 <style lang="scss" scoped>
     #cart{
         min-height: calc(100vh - 50px);
-        background-color: rgb(188, 33, 33);
+        background-color: #F6E7C1;
+
+        .jumbotron{
+            height: 600px;
+            background-image: url('../../../image/delivery-1.png');
+            background-repeat: no-repeat;
+            background-size: cover;
+            position: relative;
+
+            img{
+                width: 50%;
+                position: absolute;
+                bottom: 50%;
+                left: 50%;
+                transform: translate(-50%, 50%);
+            }
+        }
+
+        .card.cart{
+            border: none;
+            border-radius: 20px;
+            box-shadow: 0 0 5px rgb(146, 146, 146);
+        }
     }
 </style>
--- resources/js/components/Pages/RestaurantDetails.vue
+++ resources/js/components/Pages/RestaurantDetails.vue
@@ -2,21 +2,19 @@
   <main id="restaurant-details">
     <div class="container">
       <h2 class="text-white text-center p-5">{{restaurant.name}}</h2>
-      <ul class="d-flex flex-wrap justify-content-center list-unstyled">
+      <ul class="d-flex flex-wrap list-unstyled">
         <li class="p-3" v-for="dish in restaurant.dishes" :key="dish.id">
-          <div class="card" style="width: 18rem;">
+          <div class="card dish">
             <img :src="dish.image" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">{{dish.name}}</h5>
               <p class="card-text">{{dish.description}}</p>
-              <i @click="addToCart(dish)" class="fa-solid fa-plus btn btn-danger"></i>
+              <strong class="">{{dish.price}}€</strong>
+              <i @click="addToCart(dish)" class="fa-solid fa-plus d-flex justify-content-center align-items-center mt-3"></i>
             </div>
           </div>
         </li>
       </ul>
-    </div>
-    <div>
-      <p class="text-white" v-for="item in cart" :key="item.id">{{item}}</p>
     </div>
   </main>
 </template>
@@ -102,9 +100,51 @@ methods: {
 <style lang="scss" scoped>
   #restaurant-details{
     background-color: rgb(188, 33, 33);
-    
-    i{
-      cursor: pointer;
+    padding: 50px;
+
+    h2{
+      font-size: 46px;
+      text-shadow: 0 0 5px rgb(39, 39, 39);
     }
+
+    ul{
+      li{
+        width: 25%;
+        .card.dish{
+          width: 25;
+          border-radius: 20px;
+          border: none;
+
+          img{
+            border-radius: 20px 20px 0 0;
+          }
+
+          strong{
+            font-size: 22px;
+          }
+
+          i{
+          cursor: pointer;
+          border-radius: 50%;
+          border: 2px solid rgb(188, 33, 33);
+          color: rgb(188, 33, 33);
+          display: inline-block;
+          width: 50px;
+          height: 50px;
+          font-size: 20px;
+          transition: 0.5s;
+
+          &:hover{
+            background-color: rgb(188, 33, 33);
+            color: white;
+          }
+        }
+        }
+      }
+    }
+    
+    
+    
+
   }
 </style>
