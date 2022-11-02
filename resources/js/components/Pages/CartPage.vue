ba485a888159c4ff00453cfebaeac13a462a1120
simonebonifazi <simonbonifraz@gmail.com>
2022-11-02 03:20:04 +0100

    tryed to change db datas by axios

--- resources/js/components/Pages/CartPage.vue
+++ resources/js/components/Pages/CartPage.vue
@@ -13,6 +13,7 @@
                         <th scope="col">Prezzo</th>
                         <th scope="col">Quantità</th>
                         <th scope="col">Sub-totale</th>
+                        <th scope="col">Totale</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -22,8 +23,9 @@
                         <td>{{dish.price}}€</td>
                         <td><input @change="getCurrentQuantity(dish)" type="number" step="1" min="1" max="50" :value="dish.quantity" id="quantity"></td>
                         <td>{{getSubTotal(dish)}}€</td>
-                        </tr>
-                    </tbody>
+                        <td >€</td>
+                    </tr>
+                </tbody>
                 </table>
                 <div class="d-flex justify-content-between">
                     <i class="fa-solid fa-rotate-left btn btn-primary updated">Aggiorna carrello</i>
@@ -34,24 +36,24 @@
             <div class="card cart p-5 mt-5">
                 <form id="checkout-form">
                     <div class="form-group">
-                        <label for="name">Nome e cognome</label>
-                        <input type="text" class="form-control" id="name">
+                        <label for="customer_name">Nome e cognome</label>
+                        <input v-model="form.customer_name" type="text" class="form-control" id="customer_name">
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-6">
-                            <label for="email">Indirizzo email</label>
-                            <input type="email" class="form-control" id="email">
+                            <label for="customer_email">Indirizzo email</label>
+                            <input v-model="form.customer_email" type="email" class="form-control" id="customer_email">
                         </div>
                         <div class="form-group col-md-6">
-                            <label for="phone">Numero di telefono</label>
-                            <input type="phone" class="form-control" id="phone">
+                            <label for="customer_phone">Numero di telefono</label>
+                            <input v-model="form.customer_phone" type="text" class="form-control" id="customer_phone">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="address">Indirizzo completo</label>
-                        <input type="text" class="form-control" id="address">
+                        <label for="customer_address">Indirizzo completo</label>
+                        <input v-model="form.customer_address" type="text" class="form-control" id="customer_address">
                     </div>
-                    <button type="submit" class="btn btn-primary">Ordina</button>
+                    <button @click.prevent="catchData()" type="submit" class="btn btn-primary">Ordina</button>
                 </form>
             </div>
         </div>
@@ -59,11 +61,19 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default{
     name: 'CartPage',
     data(){
         return{
             cart: [],
+            form:{
+                customer_name: '',
+                customer_email: '',
+                customer_phone: '',
+                customer_address: '',
+            },
+            order: [], //deve diventare cart
         }
     },
     methods:{
@@ -77,6 +87,7 @@ export default{
         getSubTotal(dish){
             let subTotal = 0;
             subTotal = dish.price * dish.quantity;
+            
             return subTotal;
         },
         getCurrentQuantity(dish){
@@ -92,13 +103,61 @@ export default{
               } else {
                 die();
               }
-        }
+        },
+        // getAmount(cart){
+        //     let total = 0;
+        //     for(let i = 0; i < this.cart.length; i++){
+        //         total += this.cart.dish.price * this.cart.dish.quantity;
+
+        //     };
+        //     console.log(cart);
+        // },
+
+        catchData(){
+         
+            axios.post('http://127.0.0.1:8000/api/orders', {
+                customer_name: this.form.customer_name,//??
+                customer_email: this.form.customer_email,
+                customer_phone: this.form.customer_phone,
+                customer_address: this.form.customer_address,
+            }).then(res => {
+                //fill the order with the form data, way1
+                    //dati del form
+                    const formData = res.config['data'];
+                    const parsedData = JSON.parse(formData);
+                    //array del new_order che arriva da db
+                const newOrder = res.data['new_order'];
+                     console.log(res);
+                     console.log(res.data);
+                    //console.log(res.data[0]);
+                    console.log(res.data['new_order']);
+                    // ci carico i dati 
+                    newOrder.push(parsedData);
+                this.order.push(newOrder);
+                console.log(this.order);
+                //fill the order with the form data, way2
+            //     const data = res.data;
+              
+                //ora ho un oggett nell'ordine contenente il nuovo ordine, vuoto
+               
+            });
+        },
     },
     mounted(){
         if(localStorage.cart){
             this.cart = JSON.parse(localStorage.cart);
         }
     },
+    // created(){
+    //     this.$http.get('http://127.0.0.1:8000/api/user-details', {
+    //         name: this.form.name,
+    //         email: this.form.email,
+    //         phone: this.form.phone,
+    //         address: this.form.address,
+    //     }).then(function (data) {
+    //         console.log(data)
+    //     });
+    // },
     watch:{
         cart(newCart){
             localStorage.cart = JSON.stringify(newCart);
